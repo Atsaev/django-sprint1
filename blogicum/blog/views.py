302148a2bd44d731c89ee61f27d015d0fd89c487
Khasan Atsaev <atsaev.khasan@yandex.ru>
2025-04-20 11:33:19 +0300

    urls connected

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    template = 'blog/index.html'
+    ##context = {'ice_cream': ice_cream_catalog[pk]}
+    return render(request, template)
+
+def detail(request):
+    template = 'blog/detail.html'
+    return render(request, template)
+
+def category(request):
+    template = 'blog/categroy.html'
+    return render(request, template)
